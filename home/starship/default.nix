bbecc082e980ab30f2689cdbe1270de1ed1a4ced
squaredx <jason.wolfe71@gmail.com>
2025-06-30 10:46:45 -0600

    feat: add starship

---
+++ home/starship/default.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    starship
+  ];
+
+  xdg.configFile."starship.toml".source = ./starship.toml;
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+  }
+}
